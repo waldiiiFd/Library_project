d506b6131f2dea8bd644306245fd94b7be84c8c8
Waldo Fernandez <waldocu02@gmail.com>
2025-04-03 15:19:02 -0400

    feat: add reports api routes

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\FineController;
 use App\Http\Controllers\LoanController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\SearchHistoryController;
 use App\Http\Controllers\UserController;
@@ -18,15 +19,21 @@ use Illuminate\Support\Facades\Route;
 })->middleware('auth:sanctum');
  */
 
- Route::resource('/authors', AuthorController::class); // GET, POST, PUT, DELETE
- Route::resource('/books', BookController::class); // GET, POST, PUT, DELETE
- Route::resource('/categories', CategoryController::class); // GET, POST, PUT, DELETE
- Route::resource('/fines', FineController::class); // GET, POST, PUT, DELETE
- Route::resource('/loans', LoanController::class); // GET, POST, PUT, DELETE
- Route::resource('/publishers', PublisherController::class); // GET, POST, PUT, DELETE
- Route::resource('/reservations', ReservationController::class); // GET, POST, PUT, DELETE
- Route::resource('/search_histories', SearchHistoryController::class); // GET, POST, PUT, DELETE
- Route::resource('/users', UserController::class); // GET, POST, PUT, DELETE
- Route::resource('book_category', BookCategoryController::class); // GET, POST, PUT, DELETE
- Route::resource('author_book', AuthorBookController::class); // GET, POST, PUT, DELETE
+Route::resource('/authors', AuthorController::class); // GET, POST, PUT, DELETE
+Route::resource('/books', BookController::class); // GET, POST, PUT, DELETE
+Route::resource('/categories', CategoryController::class); // GET, POST, PUT, DELETE
+Route::resource('/fines', FineController::class); // GET, POST, PUT, DELETE
+Route::resource('/loans', LoanController::class); // GET, POST, PUT, DELETE
+Route::resource('/publishers', PublisherController::class); // GET, POST, PUT, DELETE
+Route::resource('/reservations', ReservationController::class); // GET, POST, PUT, DELETE
+Route::resource('/search_histories', SearchHistoryController::class); // GET, POST, PUT, DELETE
+Route::resource('/users', UserController::class); // GET, POST, PUT, DELETE
+Route::resource('book_category', BookCategoryController::class); // GET, POST, PUT, DELETE
+Route::resource('author_book', AuthorBookController::class); // GET, POST, PUT, DELETE
 
+Route::prefix('reports')->group(function () {
+    Route::get('/popular-books', [ReportController::class, 'popularBooks']);
+    Route::get('/defaulters', [ReportController::class, 'usersWithFines']);
+    Route::get('/stadistics', [ReportController::class, 'loansByCategory']);
+    Route::get('/efficiency-returns', [ReportController::class, 'returnEfficiency']);
+});
